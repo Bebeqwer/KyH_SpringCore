eb1e130ecd15443935a9286c7c18eeb11db25c58
Seorim <rehiter48@gmail.com>
2023-06-20 10:42:26 +0900

    Initial commit

---
+++ src/main/java/hellob/corec/CorecApplication.java
@@ -0,0 +1,13 @@
+package hellob.corec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellob/corec/CorecApplicationTests.java
@@ -0,0 +1,13 @@
+package hellob.corec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
